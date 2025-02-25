b48a07389d1e72e7531b4dcedeca2f3c9a3bf635
yahuaiii0923 <D00275358@student.dkit.ie>
2025-02-25 11:18:54 +0000

    parse line and load data

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 // Created by Siew Yahuai on 24/02/2025.
 //
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <fstream>
 #include <vector>
 using namespace std;
@@ -12,7 +14,45 @@ struct Product {
     double unit_price;
     int quantity;
     string supplier;
+};
+
+void parseLine(const string &line, Product &prod) {
+    string temp;
+    stringstream ss(line);
+
+    //get product id
+    getline(ss, temp, ',');
+    prod.product_id = stoi(temp);
+
+    //get product name
+    getline(ss, prod.product_name, ',');
+
+    //get product unit price
+    getline(ss, temp, ',');
+    prod.unit_price = stod(temp);
+
+    //get product quantity
+    getline(ss, temp, ',');
+    prod.quantity = stoi(temp);
+
+    //get product supplier
+    getline(ss,prod.supplier, ',');
+}
+
+void load(const string &fname, vector<Product> &prod) {
+    ifstream fin(fname);
+    if (fin) {
+        string line;
+        while (getline(fin, line)) {
+            Product products;
+            parseLine(line, products);
+            prod.push_back(products);
+        }
+        fin.close();
+    }
 }
 
 int main() {
+    vector<Product> v;
+    load("data.csv", v);
  }
